112b5b4ac2969832cf10f657a60e32645ec9c167
Eby Sofyan Fadly <eby.sofyan@gmail.com>
2018-10-04 13:56:24 +0800

    change script.py

--- scripts/script.py
+++ scripts/script.py
@@ -7,3 +7,7 @@ print('Anything You Want')
 # AUTHOR: rozalii
 # GITHUB: https://github.com/rozalii
 print('Izaq R')
+
+# AUTHOR: ebysofyan
+# GITHUB: https://github.com/ebysofyan
+print('Hello World')
